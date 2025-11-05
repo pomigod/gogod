eb937e7dfc349f8758301e59233039a3272a50a7
temp <temp@temp.com>
2025-11-05 20:14:35 +0900

    Add AI problems

---
+++ AI_TOP_100/ai_top_100_ocr 2번/decoded_code.c
@@ -0,0 +1,24 @@
+#include<stdio.h>//char*c_s="S";printf(c_s);int r=lambda x:input("TURNAROUND");
+a="G";i=input;a+="NO";q="TL";b="R";b+="W";a+=b;q+="AH";q=q[::1];a=a[::1];p=lambda x:print(x,end=b[:0])or(exit());""";char*c_e="E";
+void(p)(char*x){printf(x);};int(main)(){char*c_e="E";p(c_e);char*c_q="Q";p(c_q);char*c_x="X";p(c_x);p("CURSED");char*c_u="";
+if(i()!=q)
+                                                                                                :p(a);83j;
+eval;#char                                                                                      c[8888];c_s
+8//8;eval;        hex;j=         input;         r="A";          i=input;           8;i=input;p=print;8;         c[0];eval;
+eval;8//8;        8;r=           "H";           False;          r="L";             888;r="T";r+="Q";88;        ([]);eval;
+eval;([]);        r=r+           "C";           i=input;        88j;                                  8+8;      8//8;eval;
+8**8;8**8;        r=r+           "T";           p=print;        8+8;                                  8+3;      ([]);8//8;
+([]);([]);        r=r+           "O";           r=r+    "Z";    8+3;                                  888;      open;8**8;
+8**8;8**8;        p=print;r+="S";88;            r=r+    "B";    888;                                  83j;      8//8;open;
+8//8;eval;        True;r+="E";False;            r=r+    "S";    83j;                                  8+3;      eval;8//8;
+8**8;8//8;        r=r+           "X";           r=r+    "T";    len;                                  8+8;      eval;([]);
+8**8;8//8;        r=r+           "V";           abs;r+="R";88;  8+8;                                  8/8;      ([]);([]);
+8//8;open;        r=r+           "C";           83j;r="A";838;  8/8;                                  8/3;      open;eval;
+open;8//8;        r=r+           "I";           r=r+    "T";    8/3;                                  88j;      open;([]);
+([]);eval;        r=r+           "Q";           r=r+    "P";    p=print;r+="1";8+8;                   8+8;      ([]);([]);
+8**8;8**8;        r+="0"         ;pass;         8;pass  ;pass;  8j;r+="0";8//8;pass;                  38;p(r);  open;([]);
+eval;([]);                                                                                                      ([]);8//8;
+eval;eval;                                                                                                      open;8//8;
+([]);([]);([]);8**8;([]);eval;8**8;([]);eval;8**8;open;8**8;open;eval;eval;8//8;8**8;eval;8//8;8//8;([]);8**8;8//8;([]);open;8//8;
+eval;8**8;([]);([]);eval;eval;open;([]);eval;8//8;8//8;8//8;([]);open;eval;([]);eval;eval;eval;8//8;open;8**8;8//8;([]);eval;
+8//8;open;8**8;8//8;open;eval;8**8;8//8;eval;8**8;8//8;8//8;8//8;([]);8**8;8//8;open;8**8;8**8;8//8;8//8;8;open;([]);([]);eval;
